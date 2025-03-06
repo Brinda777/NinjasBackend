8e3716c16af63114e8b3a47ba8ec4c3b2df15543
Brinda Bhattarai <brindaabrindaa7@gmail.com>
2025-03-06 12:07:30 +0545

    Updates on Providers

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,48 +1,52 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, ParseIntPipe, Post, Put, Query, ValidationPipe } from '@nestjs/common';
 import { CreateNinjaDto } from './dto/create-ninja.dto';
 import { UpdateNinjaDto } from './dto/update-ninja.dto';
+import { NinjasService } from './ninjas.service';
 
+//Dependency Injection
+//const service = new NinjasService();
+//const controller = new NinjasController(service);
 @Controller('ninjas')
 export class NinjasController {
-    //GET /ninjas?type=fast --> []
+    constructor(private readonly ninjasService: NinjasService) {
+        
+    }
+    //GET /ninjas?weapon=fast --> []
     @Get()
-    getNinjas(@Query('type') type: string) {
-        return [{type}];
+    getNinjas(@Query('weapon') weapon: 'stars' | 'nunchucks') {
+        //const service = new NinjasService();
+        return this.ninjasService.getNinjas(weapon);
     }
 
     //GET / ninjas/:id --> { ... }
     @Get(':id')
-    getOneNinja(@Param('id') id: string) {
-        console.log('-----------', id)
-        return {
-            id,
-        };
+    getOneNinja(@Param('id', ParseIntPipe) id: number) {  //pipes
+        try {
+            return this.ninjasService.getNinja(id);
+        } catch (err) {
+            throw new NotFoundException();
+        }
+        
+
+            
     }
 
     //POST /ninjas
     @Post()
-    createNinja(@Body() createNinjaDto: CreateNinjaDto ) {
-        return {
-            name: createNinjaDto.name,
-        };
+    createNinja(@Body(new ValidationPipe()) createNinjaDto: CreateNinjaDto ) {
+        return this.ninjasService.createNinja(createNinjaDto);
     }
 
     //PUT ninjas/:id --> { ... }
     @Put(':id')
     updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
-        return {
-            id,
-            name: updateNinjaDto,
-        };
+        return this.ninjasService.updateNinja(+id, updateNinjaDto);
     }
 
     //Delete /ninjas/:id
     @Delete(':id')
     removeNinja(@Param('id') id: string) {
-        return {
-            id,
-        };
-
+        return this.ninjasService.removeNinja(+id)
         
     }
 
